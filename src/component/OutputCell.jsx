b17c862af89e9c18a4b20232f58215dcad1e4b07
ike04 <boc.0409@outlook.jp>
2023-09-19 19:17:13 +0900

    ローディングを追加

--- src/component/OutputCell.jsx
+++ src/component/OutputCell.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import useMedia from "use-media";
 import Grid from '@mui/material/Unstable_Grid2';
+import { CircularProgress } from '@mui/material';
 
 const textStyle = {
     textAlign: 'left',
@@ -30,21 +31,23 @@ const outputStyle = {
 
 const OgpFetcher = (props) => {
     const isWide = useMedia({ minWidth: "480px" });
+    const [progress, setProgress] = React.useState(false);
     const [ogp, setOgp] = useState(null);
-    const url = props.url;
     const [date, setDate] = useState(null);
+    const url = props.url;
 
     function click() {
-        window.location.href = url
+        window.open(url, '_blank')
         window.gtag("event", "techblog", {
             event_category: "click",
             event_label: "output",
-          });
+        });
     };
 
     useEffect(() => {
         const fetchOgp = async () => {
             try {
+                setProgress(true)
                 const response = await axios.get(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`);
                 const html = response.data.contents;
                 const parser = new DOMParser();
@@ -57,11 +60,13 @@ const OgpFetcher = (props) => {
                 };
                 setOgp(ogp);
                 setDate(formatPublishedDate(ogp.publishedDate));
+                setProgress(false)
             } catch (error) {
+                setProgress(false)
                 console.error('Error fetching OGP:', error);
             }
         }
-    
+
         const formatPublishedDate = (rawDate) => {
             if (rawDate) {
                 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
@@ -74,16 +79,32 @@ const OgpFetcher = (props) => {
                 return '';
             }
         }
-        
-    
+
+
         fetchOgp();
     }, []);
 
     return (
         <>
-            {ogp && (
-                isWide ?
-                    <Grid xs={6} >
+            {progress ? <CircularProgress align="center"/> :
+                ogp && (
+                    isWide ?
+                        <Grid xs={6} >
+                            <div style={outputStyle} onClick={() => click()}
+                                role="button"
+                                tabIndex="0"
+                                onKeyDown={(e) => {
+                                    if (e.key === "Enter" || e.key === " ") {
+                                        // Enter or Space で実行
+                                        click();
+                                    }
+                                }} >
+                                <img src={ogp.image} style={{ width: '100%', borderTopLeftRadius: '10px', borderTopRightRadius: '10px' }} />
+                                <h3 style={textStyle}>{ogp.title}</h3>
+                                <p style={textStyle}>{date}</p>
+                            </ div>
+                        </Grid>
+                        :
                         <div style={outputStyle} onClick={() => click()}
                             role="button"
                             tabIndex="0"
@@ -97,22 +118,8 @@ const OgpFetcher = (props) => {
                             <h3 style={textStyle}>{ogp.title}</h3>
                             <p style={textStyle}>{date}</p>
                         </ div>
-                    </Grid>
-                    :
-                    <div style={outputStyle} onClick={() => click()}
-                        role="button"
-                        tabIndex="0"
-                        onKeyDown={(e) => {
-                            if (e.key === "Enter" || e.key === " ") {
-                                // Enter or Space で実行
-                                click();
-                            }
-                        }} >
-                        <img src={ogp.image} style={{ width: '100%', borderTopLeftRadius: '10px', borderTopRightRadius: '10px' }} />
-                        <h3 style={textStyle}>{ogp.title}</h3>
-                        <p style={textStyle}>{date}</p>
-                    </ div>
-            )}
+                )}
+
         </>
     );
 }
